1214420dfef3e052b7d229571ae894df1f0003cb
Rational-pi <1salmieri.nicolas@gmail.com>
2018-01-22 14:05:28 +0100

    new arduino dummy funcs

--- dummyHeaders/Arduino.h
+++ dummyHeaders/Arduino.h
@@ -3,7 +3,16 @@
 #include "inttypes.h"
 
 #define INPUT 0
+
+// interupt modes
 #define LOW 0
+#define CHANGE 0
+#define RISING 0
+#define FALLING 0
+#define HIGH 0
+
+
+
 uint8_t digitalRead(uint8_t DT){return DT;}
 void pinMode(uint8_t pin, uint8_t mode){}
 void attachInterrupt(uint8_t InPin, void(*)()  , uint8_t mode){}
